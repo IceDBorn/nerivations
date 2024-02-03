9d5a831775c517c758f96267881266103f1e7c91
IceDBorn <git.outsider841@simplelogin.fr>
2024-02-03 22:03:54 +0200

    Fix cascade path

--- modules/overlay.nix
+++ modules/overlay.nix
@@ -2,7 +2,7 @@ final: super:
 
 let c = p: final.callPackage p { };
 in {
-  firefox-cascade = c ../pkgs/firefox-gnome-theme.nix;
+  firefox-cascade = c ../pkgs/firefox-cascade.nix;
   firefox-gnome-theme = c ../pkgs/firefox-gnome-theme.nix;
   nvchad = c ../pkgs/nvchad.nix;
   proton-ge = c ../pkgs/proton-ge.nix;
