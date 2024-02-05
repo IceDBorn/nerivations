5a633c3064b0c4ae97904a894d758694e502aa04
IceDBorn <git.outsider841@simplelogin.fr>
2024-02-05 10:26:22 +0200

    firefox-gnome-theme: 121.1 -> 122

--- pkgs/firefox-gnome-theme.nix
+++ pkgs/firefox-gnome-theme.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   name = "firefox-gnome-theme";
-  version = "121.1";
+  version = "122";
 
   src = fetchFromGitHub {
     owner = "rafaelmardojai";
     repo = "firefox-gnome-theme";
     rev = "v${version}";
-    sha256 = "SYp0DRkO73i8XVyOdAlcP2ZItqx9DqraIEJy6mY/2Ng=";
+    hash = "sha256-QZk/qZQVt1X53peCqB2qmWhpA3xtAVgY95pebSKaTFU=";
   };
 
   preferLocalBuild = true;
