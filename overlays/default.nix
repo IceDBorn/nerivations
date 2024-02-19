55a6ee1f389fd92d460801e1eb60c528c4edfd29
IceDBorn <git.outsider841@simplelogin.fr>
2024-02-19 22:00:35 +0200

    rpcs3: build with wayland support

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,6 @@
 {
   nixpkgs.overlays = [
-    (import ./rpcs3.nix) # Needed for RPCN to work
+    (import ./rpcs3.nix) # Use until nixpkgs updates to version 30 or higher
     (import ./cemu.nix) # https://nixpk.gs/pr-tracker.html?pr=289867
   ];
 }
--- overlays/rpcs3.nix
+++ overlays/rpcs3.nix
@@ -3,11 +3,13 @@ final: super: {
     src = super.fetchFromGitHub {
       owner = "rpcs3";
       repo = "rpcs3";
-      rev = "d71f4b33d75c70594449dbb3cdfc69506512c842";
-      hash = "sha256-Vz7ibmSYGcCIM6xX2DsULSH7fI/JbI+T0ickLteCNLg=";
+      rev = "7abc5f3eadcfc9b3875910efe124942ebac0c4ea";
+      hash = "sha256-4fSq7fxjadbIhAceQdKgA+CBM1qptC5ImeaBycYGFL8=";
       fetchSubmodules = true;
     };
 
-    version = "0.0.30-16100";
+    version = "0.0.30-16122";
+
+    buildInputs = superAttrs.buildInputs ++ [ super.qt6.qtwayland ];
   });
 }
