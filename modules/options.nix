ed91c5260572675184722e197e0193f8a57086f5
IceDBorn <git.outsider841@simplelogin.fr>
2024-02-22 10:59:16 +0200

    config: add option for overlays

--- modules/options.nix
+++ modules/options.nix
@@ -10,6 +10,11 @@
         type = types.str;
         default = "-e library/hide_whats_new -e login/hover_qr";
       };
+
+      overlays.enable = mkOption {
+        type = types.bool;
+        default = true;
+      };
     };
   };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,8 @@
-{
-  nixpkgs.overlays = [
+{ config, lib, ... }:
+
+let cfg = config.nerivations.overlays;
+in {
+  nixpkgs.overlays = lib.mkIf cfg.enable [
     (import ./rpcs3.nix) # Use until nixpkgs updates to version 30 or higher
     (import ./cemu.nix) # https://nixpk.gs/pr-tracker.html?pr=289867
   ];
