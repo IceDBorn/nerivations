ec036a573f7a6fd518f6569fca72580e9c74aae3
IceDBorn <git.outsider841@simplelogin.fr>
2024-03-08 20:57:59 +0200

    cemu: remove overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,6 +4,5 @@ let cfg = config.nerivations.overlays;
 in {
   nixpkgs.overlays = lib.mkIf cfg.enable [
     (import ./rpcs3.nix) # Use until nixpkgs updates to version 30 or higher
-    (import ./cemu.nix) # https://nixpk.gs/pr-tracker.html?pr=289867
   ];
 }
