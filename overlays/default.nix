c620a2f95abe1c1e753e75cfed699ad6f5acc559
IceDBorn <git.outsider841@simplelogin.fr>
2024-03-20 10:17:18 +0200

    overlays: add waybar

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,5 +4,6 @@ let cfg = config.nerivations.overlays;
 in {
   nixpkgs.overlays = lib.mkIf cfg.enable [
     (import ./rpcs3.nix) # Use until nixpkgs updates to version 30 or higher
+    (import ./waybar.nix) # https://nixpk.gs/pr-tracker.html?pr=297158
   ];
 }
