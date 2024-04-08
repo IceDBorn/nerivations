1ae9b3f62b306e95ff30db3767569c9d382fe037
IceDBorn <git.outsider841@simplelogin.fr>
2024-04-08 19:05:05 +0300

    overlays: remove rpcs3

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,8 +1,8 @@
 { config, lib, ... }:
 
-let cfg = config.nerivations.overlays;
-in {
-  nixpkgs.overlays = lib.mkIf cfg.enable [
-    (import ./rpcs3.nix) # https://nixpk.gs/pr-tracker.html?pr=299971
-  ];
+let
+  cfg = config.nerivations.overlays;
+in
+{
+  nixpkgs.overlays = lib.mkIf cfg.enable [ ];
 }
