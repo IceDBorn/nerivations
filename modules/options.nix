ffe058b9891d9a296e79ebc4dbf851fd6df7c961
IceDBorn <git.outsider841@simplelogin.fr>
2024-04-13 20:53:21 +0300

    pkgs: remove adwaita for steam

--- modules/options.nix
+++ modules/options.nix
@@ -1,11 +1,8 @@
-{ lib, ... }: {
+{ lib, ... }:
+
+{
   options = with lib; {
     nerivations = {
-      adwaita-for-steam.extras = mkOption {
-        type = types.str;
-        default = "-e library/hide_whats_new -e login/hover_qr";
-      };
-
       overlays.enable = mkOption {
         type = types.bool;
         default = true;
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,19 +1,18 @@
 { config, ... }:
 
 let
-  extras = config.nerivations.adwaita-for-steam.extras;
   userjs = config.nerivations.arkenfox-userjs.userjs;
-in {
+in
+{
   nixpkgs.overlays = [
-    (final: super:
+    (
+      final: super:
       let
         c = p: final.callPackage p { };
         k = p: config.boot.kernelPackages.callPackage p { };
-      in {
-        adwaita-for-steam =
-          final.callPackage ./adwaita-for-steam { inherit extras; };
-        arkenfox-userjs =
-          final.callPackage ./arkenfox-userjs.nix { inherit userjs; };
+      in
+      {
+        arkenfox-userjs = final.callPackage ./arkenfox-userjs.nix { inherit userjs; };
         cliphist-rofi-img = c ./cliphist-rofi-img.nix;
         deckbd = c ./deckbd.nix;
         firefox-cascade = c ./firefox-cascade.nix;
@@ -22,6 +21,7 @@ in {
         nvchad = c ./nvchad.nix;
         tpm = c ./tpm.nix;
         wine-ge = c ./wine-ge.nix;
-      })
+      }
+    )
   ];
 }
