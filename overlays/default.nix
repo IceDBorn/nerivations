e6131d452a71caf5e223f61cbda2ee6ceab14d56
IceDBorn <git.outsider841@simplelogin.fr>
2024-05-03 13:28:57 +0300

    overlays: add waybar (fixes hyprland stuff)

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,5 +4,7 @@ let
   cfg = config.nerivations.overlays;
 in
 {
-  nixpkgs.overlays = lib.mkIf cfg.enable [ ];
+  nixpkgs.overlays = lib.mkIf cfg.enable [
+    (import ./waybar.nix) # Needed until next waybar release
+  ];
 }
