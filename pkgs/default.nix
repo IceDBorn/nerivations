6906d1289a84a2eed58e4aa59a3769d4b5c6ce30
IceDBorn <git.outsider841@simplelogin.fr>
2024-10-16 16:02:28 +0300

    cliphist-rofi-img: remove package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,7 +8,6 @@
         c = p: final.callPackage p { };
       in
       {
-        cliphist-rofi-img = c ./cliphist-rofi-img.nix;
         deckbd = c ./deckbd.nix;
         hyprfreeze = c ./hyprfreeze.nix;
         nvchad = c ./nvchad.nix;
