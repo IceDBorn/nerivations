6c1529e02d4dff0be81311c0f15f94761c805a06
IceDBorn <git.outsider841@simplelogin.fr>
2024-10-16 15:59:59 +0300

    arkenfox-userjs: remove package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,18 +1,13 @@
-{ config, ... }:
+{ ... }:
 
-let
-  userjs = config.nerivations.arkenfox-userjs.userjs;
-in
 {
   nixpkgs.overlays = [
     (
       final: super:
       let
         c = p: final.callPackage p { };
-        k = p: config.boot.kernelPackages.callPackage p { };
       in
       {
-        arkenfox-userjs = final.callPackage ./arkenfox-userjs.nix { inherit userjs; };
         cliphist-rofi-img = c ./cliphist-rofi-img.nix;
         deckbd = c ./deckbd.nix;
         hyprfreeze = c ./hyprfreeze.nix;
