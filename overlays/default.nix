7059725288aa682d4136e11160fe06761bfeb9cd
IceDBorn <git.outsider841@simplelogin.fr>
2024-11-15 00:36:48 +0200

    util-linux: remove overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,7 +5,6 @@ let
 in
 {
   nixpkgs.overlays = lib.mkIf cfg.enable [
-    (self: super: { utillinux = super.util-linux; }) # https://nixpk.gs/pr-tracker.html?pr=349783
     (import ./walker.nix) # Until version 0.8.10 is in nixpkgs
   ];
 }
