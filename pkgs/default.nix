29d8c4f9d569771ec310cb944d0aee3c6f68067b
IceDBorn <git.outsider841@simplelogin.fr>
2025-01-18 01:32:53 +0200

    packages: remove nvchad reference

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,7 +9,6 @@
       in
       {
         deckbd = c ./deckbd.nix;
-        nvchad = c ./nvchad.nix;
         package-version-server = c ./package-version-server.nix;
         tpm = c ./tpm.nix;
       }
