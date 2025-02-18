20647bb3d32a1c794c283e19ac1e5b70b77f344f
IceDBorn <git.outsider841@simplelogin.fr>
2025-02-18 21:14:33 +0200

    overlays: remove tela-icon-theme

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,7 +4,5 @@ let
   cfg = config.nerivations.overlays;
 in
 {
-  nixpkgs.overlays = lib.mkIf cfg.enable [
-    (import ./tela-icon-theme.nix) # https://github.com/NixOS/nixpkgs/pull/380993
-  ];
+  nixpkgs.overlays = lib.mkIf cfg.enable [ ];
 }
