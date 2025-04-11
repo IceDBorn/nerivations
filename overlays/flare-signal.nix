290e153900a3ea151f0e8fe3b5b1017685f2554f
IceDBorn <git.outsider841@simplelogin.fr>
2025-04-11 04:21:18 +0300

    flare-signal: 0.15.15 -> 0.15.16

--- overlays/flare-signal.nix
+++ overlays/flare-signal.nix
@@ -2,19 +2,19 @@ final: super: {
   flare-signal = super.flare-signal.overrideAttrs (
     finalAttrs: superAttrs: {
       pname = "flare";
-      version = "0.15.15";
+      version = "0.15.16";
 
       src = final.fetchFromGitLab {
         domain = "gitlab.com";
         owner = "schmiddi-on-mobile";
         repo = "flare";
         rev = finalAttrs.version;
-        hash = "sha256-6U1Hq3ietB8yxq04yvhA350kP8l4t0IbIu+cLl1K7/s=";
+        hash = "sha256-oG5RHCOk/VPCUAdg/+gfw9/cErnaZBKnwzLzdaboSyY=";
       };
 
       cargoDeps = final.rustPlatform.fetchCargoVendor {
         inherit (finalAttrs) pname version src;
-        hash = "sha256-/3POHtEFu33yk+azIZe+D45ilA+03jcgq3hzrIqI4W8=";
+        hash = "sha256-Id08JgFbeuGrMrBFyXF/ke/LfkqvT+biEgDOyW8sZh0=";
       };
     }
   );
