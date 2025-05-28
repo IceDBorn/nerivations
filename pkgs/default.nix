906564aa126d2bb191f2c194f2f91440e50c8728
IceDBorn <git.outsider841@simplelogin.fr>
2025-05-28 05:02:01 +0300

    packages: add faugus-launcher

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,6 +9,7 @@
       in
       {
         deckbd = c ./deckbd.nix;
+        faugus-launcher = c ./faugus-launcher.nix; # https://nixpk.gs/pr-tracker.html?pr=402220
         hyproled = c ./hyproled.nix;
       }
     )
