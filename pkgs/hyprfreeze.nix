e8f1685b20514391c6124854462665ff629bfe52
IceDBorn <git.outsider841@simplelogin.fr>
2025-07-03 19:01:17 +0300

    hyprfreeze: git -> 1.1.2

--- pkgs/hyprfreeze.nix
+++ pkgs/hyprfreeze.nix
@@ -7,12 +7,12 @@
 
 stdenvNoCC.mkDerivation rec {
   name = "hyprfreeze";
-  version = "git-6b674166ba88a37da4136a87b4fa756d3aef601f";
+  version = "1.1.2";
 
   src = fetchFromGitHub {
     owner = "zerodya";
     repo = "hyprfreeze";
-    rev = "6b674166ba88a37da4136a87b4fa756d3aef601f";
+    tag = "v${version}";
     hash = "sha256-iMFSbMRVO3yOBZvCzx0BAb26KEETE7Nb+960B24r+W4=";
   };
 
